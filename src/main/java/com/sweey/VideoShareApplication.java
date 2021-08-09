f336d4f3962c0a914aa6a7083ca433f8948034ad
Sweey <960102512@qq.com>
2021-08-10 00:20:32 +0800

    创建项目

---
+++ src/main/java/com/sweey/VideoShareApplication.java
@@ -0,0 +1,13 @@
+package com.sweey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoShareApplication.class, args);
+	}
+
+}
